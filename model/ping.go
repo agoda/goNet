fc1dedb0cfb4ccfdb79bffb1b7fc72c1f076878c
Anvay Goda <anvaygoda@Anvays-MacBook-Pro.local>
2018-04-17 17:51:22 -0600

    Deleted cmd module, created Ping structure, and Command interface

---
+++ model/ping.go
@@ -0,0 +1,26 @@
+package model
+
+import (
+"encoding/json"
+)
+
+
+
+type Ping struct {
+	Output string `json:"output"`
+	options []string
+}
+
+func NewPing(options []string) *Ping {
+	return &Ping{
+		options: options,
+	}
+}
+
+func (this *Ping) Run() (bool,error){
+
+}
+
+func (this *Ping) GetResults() ([]byte,error) {
+	
+}
