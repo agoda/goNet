09f64fe7374804cffb94504441bc8323263b6c77
Anvay Goda <anvaygoda@Anvays-MacBook-Pro.local>
2018-04-18 14:39:02 -0600

    Created ping logic: implemented Run() and GetResults()

--- model/ping.go
+++ model/ping.go
@@ -20,8 +20,22 @@ func NewPing(options []string) *Ping {
 
 func (this *Ping) Run() (bool,error){
 
+	cmd := this.options[0]
+	countFlag := this.options[1]
+	count := this.options[2]
+	target := this.options[3]
+
+	os_cmd := exec.Command(cmd,countFlag,count,target)
+	out,err := os_cmd.Output()
+
+	if err != nil {
+		return false, err
+	} else {
+		this.Output = string(out)
+		return true, nil
+	}
 }
 
 func (this *Ping) GetResults() ([]byte,error) {
-	
+	return json.Marshal(this)
 }
